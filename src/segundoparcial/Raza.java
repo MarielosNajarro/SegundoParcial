c86fa4a28d876f4001c981d10909b1ef8b686f8b
Marielos Najarro <rf.aparicio@gmail.com>
2018-06-06 06:36:59 -0600

    Agregando la clase Raza

---
+++ src/segundoparcial/Raza.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundoparcial;
+
+/**
+ *
+ * @author marielos
+ */
+public class Raza {
+//Atributos de la clase
+    private String nombre_raza;
+    private CentroDeMando mando;
+//Contructor de la clase
+    public Raza(String nombre_raza, String nombre_mando) {
+        this.nombre_raza = nombre_raza;
+        mando = new CentroDeMando(nombre_mando);
+    }
+//Metodo de mostrar informacion  
+    public void mostrarInformacion(){
+        System.out.println(nombre_raza.toUpperCase());
+        mando.mostrarRecursos();
+    }
+    
+    public void asignar_recurso_a(String nombre_a, int valor){
+        mando.setRecurso_a(new Recurso(nombre_a, valor));
+    }
+    public void asignar_recurso_b(String nombre_b, int valor){
+        mando.setRecurso_b(new Recurso(nombre_b, valor));
+    }
+    public void asignar_recurso_c(String nombre_c, int valor){
+        mando.setRecurso_c(new Recurso(nombre_c, valor));
+    }
+//Get y set
+    public String getNombre_raza() {
+        return nombre_raza;
+    }
+
+    public void setNombre_raza(String nombre_raza) {
+        this.nombre_raza = nombre_raza;
+    }
+
+    public CentroDeMando getMando() {
+        return mando;
+    }
+
+    public void setMando(CentroDeMando mando) {
+        this.mando = mando;
+    }
+}

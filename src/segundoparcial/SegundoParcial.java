673d7b8b76bbedac9d13093df16634bad4c32d14
Marielos Najarro <rf.aparicio@gmail.com>
2018-06-06 06:38:49 -0600

    Pruebas

--- src/segundoparcial/SegundoParcial.java
+++ src/segundoparcial/SegundoParcial.java
@@ -15,7 +15,24 @@ public class SegundoParcial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Raza alianza = new Raza("Alianza", "Cuartel");
+        alianza.asignar_recurso_a("Hielo", 1000);
+        alianza.asignar_recurso_b("Fuego", 3000);
+        alianza.asignar_recurso_c("Agua", 4000);
+        
+        alianza.getMando().getRecurso_b().gastar(400);
+        alianza.getMando().getRecurso_b().recolectar(100);
+        
+        alianza.mostrarInformacion();
+        
+        Raza horda = new Raza("Horda", "Palacio");
+        horda.asignar_recurso_a("Tierra", 1000);
+        horda.asignar_recurso_b("Energia", 1000);
+        horda.asignar_recurso_c("Poder", 1000);
+        
+        horda.getMando().getRecurso_b().gastar(1);
+        horda.getMando().subirDeNivel();
+        horda.mostrarInformacion();
     }
     
 }
